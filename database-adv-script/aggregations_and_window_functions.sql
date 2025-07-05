b5edd7942edc70040a03f70cc14b25755d898701
Agape-pr <mugishaagapelearning@gmail.com>
2025-07-05 16:40:35 +0800

    ff

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,15 +9,12 @@ GROUP BY u.user_id, u.first_name, u.last_name
 ORDER BY total_bookings DESC;
 
 
-
-
-
 SELECT
     p.property_id,
     p.property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_num
 FROM Properties p
 LEFT JOIN Bookings b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.property_name
-ORDER BY booking_rank;
+ORDER BY booking_row_num;
